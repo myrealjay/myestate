5c87e0f94f6357ee175417d006deb19e81c1db05
myrealjay <nwan.james@okbcoy.com.ng>
2019-02-12 12:19:03 +0100

    fixed favicon

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -25,6 +25,7 @@
 
     <!-- Ionicons -->
     <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
+    <link rel="icon" href="images/favicon.png" type="image/png">
 
     @yield('css')
 
